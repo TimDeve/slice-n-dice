468ba181397950e7c91285a0d685e38851ac51b4
Tim Dévé <TimDeve@users.noreply.github.com>
2022-08-16 18:59:44 +0100

    Allows caching of static resources

--- gateway/main.go
+++ gateway/main.go
@@ -42,7 +42,7 @@ func main() {
 
 	n := negroni.New()
 	n.Use(negroni.NewRecovery())
-	n.Use(noCacheStatic(negroni.NewStatic(http.Dir("public"))))
+	n.Use(negroni.NewStatic(http.Dir("public")))
 	n.Use(negroni.NewLogger())
 	n.UseHandler(r)
 
@@ -131,15 +131,6 @@ func restricted(handler http.HandlerFunc) http.HandlerFunc {
 
 func restrictedProxy(target string) http.HandlerFunc {
 	return restricted(proxy(target))
-}
-
-func noCacheStatic(s *negroni.Static) negroni.HandlerFunc {
-	return negroni.HandlerFunc(func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-		w.Header().Set("Cache-Control", "max-age=0, no-cache, no-store, must-revalidate")
-		w.Header().Set("Pragma", "no-cache")
-		w.Header().Set("Expires", "0")
-		s.ServeHTTP(w, r, next)
-	})
 }
 
 func serve(handler http.Handler) {
